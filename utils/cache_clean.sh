b44f7b357cf1ed624ea0a6225d66a373af0c92cd
kudanai <kudanai@gmail.com>
2012-08-21 19:40:57 +0800

    easy peasy installer pkg
    created a PackageMaker template for easy installation.

--- utils/cache_clean.sh
+++ utils/cache_clean.sh
@@ -1,6 +1,4 @@
-rm -rf ~/Library/Preferences/com.apple.DictionaryServices.plist
-rm -rf ~/Library/Preferences/com.apple.Dictionary.plist
-rm -rf ~/Library/Caches/com.apple.DictionaryApp
-rm -rf ~/Library/Caches/com.apple.DictionaryManager
-rm -rf ~/Library/Caches/com.apple.Dictionary
-rm -rf ~/Library/Caches/com.apple.DictionaryServices
+#!/bin/sh
+
+rm -rf ~/Library/Preferences/com.apple.Dictionary*
+rm -rf ~/Library/Caches/com.apple.Dictionary*
